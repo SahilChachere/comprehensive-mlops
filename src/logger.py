d3729619d717c32ececde3bf092e2eadeb9f02b3
Sahil Chachere <sahilchachere8@gmail.com>
2025-01-18 13:27:19 +0530

    feat: added StreamHandler for console logs

--- src/logger.py
+++ src/logger.py
@@ -2,14 +2,27 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+# Create a custom logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Create handlers
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+console_handler = logging.StreamHandler()
+
+# Create formatters and add them to handlers
+formatter = logging.Formatter(
+    "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"
 )
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
