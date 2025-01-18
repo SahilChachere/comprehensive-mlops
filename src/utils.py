0b737c1025c8a7e0dee4b73f3639c2a37b4f1a94
Sahil Chachere <sahilchachere8@gmail.com>
2025-01-18 13:27:52 +0530

    code format with black and isort

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 import os
 import pickle
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 import dill
 import numpy as np
